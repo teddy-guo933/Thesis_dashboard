fee640e9f0029b85b00885623ee9b41ea5b2e4f8
Zihan Guo <zihan.guo@emory.edu>
2026-04-25 16:26:31 -0400

    Cleanup: remove unwanted files based on .gitignore

--- Visualization.R
+++
@@ -1,8 +0,0 @@
-library(dplyr)
-library(gtsummary)
-library(haven)
-
-demographics<- read_sas("/Volumes/rsphprojects-ts/mgoodm2/DSD Pathways/Zihan Guo/Datasets/demographics_classic.sas7bdat")
-labs<-read_sas("/Volumes/rsphprojects-ts/mgoodm2/DSD Pathways/Zihan Guo/Datasets/labs_bp_final.sas7bdat")
-cah <- left_join(demographics, labs,
-                 by = c("STUDYID" = "studyid"))
